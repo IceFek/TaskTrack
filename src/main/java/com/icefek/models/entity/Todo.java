0ea92a1ac8c8e8b48d4ec085779a6c7ba67975b8
IceFek <the1shadow1knigt@gmail.com>
2025-11-22 15:15:06 +0200

    Provide Resource Interface

--- src/main/java/com/icefek/models/entity/Todo.java
+++ src/main/java/com/icefek/models/entity/Todo.java
@@ -1,9 +1,9 @@
 package com.icefek.models.entity;
 
-import com.icefek.models.notentity.Status;
+import com.icefek.models.notentity.*;
 
 import java.io.Serializable;
 
-public record Todo(long id, String description, Status status,String createdAt,String updatedAt) implements Serializable {
+public record Todo(long id, String description, Status status,String createdAt,String updatedAt) implements Serializable,Resource {
 
 }
---
+++ src/main/java/com/icefek/models/notentity/Resource.java
@@ -0,0 +1,4 @@
+package com.icefek.models.notentity;
+
+public interface Resource {
+}
